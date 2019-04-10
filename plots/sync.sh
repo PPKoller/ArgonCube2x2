ab12b1db6e4f491ef0f66c13ce80e810484db950
Patrick Koller <patrick.koller@lhep.unibe.ch>
2019-04-10 11:24:19 +0200

    Initial commit

---
+++ plots/sync.sh
@@ -0,0 +1,7 @@
+#scp lhepdaq2:DUNE/ArgonCube/Neutron/plots/png/*.png ArCube/.
+#scp lhepdaq2:DUNE/ArCLight/plots/png/*.png ArCLight/.
+scp lhepdaq2:DUNE/2x2_FNAL/plots/png/*.png 2x2/.
+
+#scp lhepdaq2:DUNE/ArgonCube/Neutron/plots/root/*.root ArCube/roots/.
+#scp lhepdaq2:DUNE/ArCLight/plots/root/*.root ArCLight/roots/.
+scp lhepdaq2:DUNE/2x2_FNAL/plots/root/*.root 2x2/roots/.
